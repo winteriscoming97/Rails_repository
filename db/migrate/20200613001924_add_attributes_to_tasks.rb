0f3c115377d9f2fe98e6725a059b7e794fa8f191
justin <justinjackson1198@gmail.com>
2020-06-13 11:45:00 -0700

    first commit

---
+++ db/migrate/20200613001924_add_attributes_to_tasks.rb
@@ -0,0 +1,7 @@
+class AddAttributesToTasks < ActiveRecord::Migration[5.2]
+  def change
+	  add_column :tasks, :content, :string
+	  add_column :tasks, :completed, :boolean, default: false
+
+  end
+end
